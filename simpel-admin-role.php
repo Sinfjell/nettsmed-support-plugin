f9504efb0c11aaf969f2f62e10f3c09ca8e9ceab
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-05-24 16:19:17 +0200

    Removed some menu pages

--- simpel-admin-role.php
+++ simpel-admin-role.php
@@ -8,8 +8,12 @@ function create_simpel_admin_role() {
     $admin_role = get_role('administrator');
     $admin_capabilities = $admin_role->capabilities;
 
-    // Remove a specific capability
-    unset($admin_capabilities['edit_others_posts']);
+    // Remove specific capabilities
+    unset($admin_capabilities['activate_plugins']);
+    unset($admin_capabilities['install_plugins']);
+    unset($admin_capabilities['update_plugins']);
+    unset($admin_capabilities['delete_plugins']);
+    unset($admin_capabilities['edit_plugins']);
 
     // Add the Simpel Admin role with the modified capabilities
     add_role(
@@ -21,11 +25,24 @@ function create_simpel_admin_role() {
 
 
 // Hide Plugins menu for Simpel Admin role
-add_action('admin_menu', 'simpel_admin_hide_menu');
+add_action('admin_menu', 'simpel_admin_hide_menu', 9999);
 function simpel_admin_hide_menu() {
     $current_user = wp_get_current_user();
     if(in_array('simpel_admin', $current_user->roles)){
         remove_menu_page('plugins.php');
+        remove_menu_page('tools.php');
+        remove_menu_page('jet-dashboard');
+        remove_menu_page('options-general.php');
+        remove_menu_page('astra');
+        remove_menu_page('complianz'); 
+        remove_menu_page('wp-mail-smtp'); 
+        remove_menu_page('jet-engine'); 
+        remove_menu_page('jet-smart-filters'); 
+
     }
+    
 }
+
+add_action('admin_menu', 'simpel_admin_hide_menu');
+
 ?>
