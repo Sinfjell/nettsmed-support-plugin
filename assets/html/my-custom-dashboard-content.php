77e4029b273e7054591b0c197a0850655145fcbe
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-05-25 09:33:09 +0200

    Edited html and css for custom dashboard

--- assets/html/my-custom-dashboard-content.php
+++ assets/html/my-custom-dashboard-content.php
@@ -1,4 +1,26 @@
 <div class="my-custom-dashboard">
-    <h1>Welcome to the Custom Dashboard!</h1>
-    <!-- Add more HTML content here -->
+  <div class="column">
+  <h1><strong>Velkommen til din nettside</strong></h1>
+
+<p>Velkommen til baksiden av nettsiden din; til ditt kontrollpanel/dashboard. Her har du tilgang til både enkle og avanserte justeringsmuligheter. Se videoene for en grunnleggende introduksjon til verktøyene dine.</p>
+
+<h2><strong>Bruk vår hjelpewidget</strong></h2>
+
+<p>På denne baksiden/dashboardet til siden din vil du alltid finne oss nede i høyre hjørne. Trykk og se et utvalg av hjelpeartikler, eller velg “Spør” og send oss en melding.</p>
+
+<h2><strong>Hurtiglenker</strong></h2>
+
+<ul>
+  <li><a href="https://tverrkirkeligno.local/wp-admin/users.php">Opprett ny bruker</a></li>
+  <li><a href="https://tverrkirkeligno.local/wp-admin/post-new.php">Legg til nytt innlegg</a></li>
+  <li><a href="https://tverrkirkeligno.local/wp-admin/nav-menus.php">Tilpass menyen din</a></li>
+</ul>
+
+  </div>
+  <div class="column">
+  <div class="intro-wordpress-video">
+<iframe title="vimeo-player" src="https://player.vimeo.com/video/797109504?h=29e41cab22" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+</div>
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/797114137?h=50a935fb0d" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+  </div>
 </div>
